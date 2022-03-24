dcb62d2fdf8d5b8cace137adfe98dc07c20bf765
Shulu Chen <rexchen129@gmail.com>
2022-03-24 16:29:57 -0400

    rearrange the codes

--- interface_naive.py
+++ interface_naive.py
@@ -25,8 +25,8 @@ class ScreenDummy(ScreenIO):
         """Just print echo messages"""
         print("BlueSky console:", text)
 
-#conpute the distance of two aircraft, meters
 def get_distance(location1,location2):
+    #compute the distance of two aircraft, meters
     lat1=location1[0]
     lon1=location1[1]
     alt1=location1[2]
@@ -37,83 +37,91 @@ def get_distance(location1,location2):
     dist=sqrt(horizon_dist**2+(alt1-alt2)**2)
     return horizon_dist
 
+def generate_demand(flight_inv,ac_num):
+    # Generate the demand based on exponential distribution,
+    # lambda-number of flight per second, lambda=0.1--flight interval=10s
+    lambda_x = 1/flight_inv
+    ac_demand_interval = [int(expovariate(lambda_x)) for i in range(ac_num)]
+    ac_depart_t = np.cumsum(ac_demand_interval)
+    ori_depart_t=ac_depart_t.copy()
+    return ac_depart_t,ori_depart_t
 
+def init_bs():
 
-f=open("scenario/interface_naive.scn","w")
+    # initialize bluesky as non-networked simulation node
+    bs.init('sim-detached')
 
-# initialize bluesky as non-networked simulation node
-bs.init('sim-detached')
+    bs.stack.stack('CRELOG NAIVE 1')
+    bs.stack.stack('NAIVE ADD id,lat, lon, alt, tas, vs ')
+    bs.stack.stack('NAIVE ON 1  ')
+    # bs.stack.stack('ASAS ON')
+    bs.stack.stack('TAXI OFF 4')
+    f.write("00:00:00.00>CRELOG NAIVE_gui 1\n")
+    f.write("00:00:00.00>NAIVE_gui ADD id,lat, lon, alt, tas, vs\n")
+    f.write("00:00:00.00>NAIVE_gui ON 1\n")
+    f.write("00:00:00.00>TRAILS ON \n")
+    f.write("00:00:00.00>TAXI OFF 4\n")
+    # f.write("0:00:00.00>ASAS ON \n")
+    f.write("0:00:00.00>PAN 0,0.2 \n")
+    f.write("0:00:00.00>ZOOM 2 \n")
+    # f.write("0:00:00.00>FF \n")
+    f.write("\n")
 
-bs.stack.stack('CRELOG NAIVE 1')
-bs.stack.stack('NAIVE ADD id,lat, lon, alt, tas, vs ')
-bs.stack.stack('NAIVE ON 1  ')
-# bs.stack.stack('ASAS ON')
-bs.stack.stack('TAXI OFF 4')
-f.write("00:00:00.00>CRELOG NAIVE_gui 1\n")
-f.write("00:00:00.00>NAIVE_gui ADD id,lat, lon, alt, tas, vs\n")
-f.write("00:00:00.00>NAIVE_gui ON 1\n")
-f.write("00:00:00.00>TRAILS ON \n")
-f.write("00:00:00.00>TAXI OFF 4\n")
-# f.write("0:00:00.00>ASAS ON \n")
-f.write("0:00:00.00>PAN 0,0.2 \n")
-f.write("0:00:00.00>ZOOM 2 \n")
-# f.write("0:00:00.00>FF \n")
-
-f.write("\n")
+    # set simulation time step, and enable fast-time running
+    bs.stack.stack('DT 1;FF')
+    bs.traf.cre(acid="A"+str(0), actype="ELE01",aclat=0.0, aclon=0.0)
+    add_plane(0)
 
 def add_plane(id):
-    speed_list=[30,31,32,33,34]
+    speed_list=[30,32,34,36,38]
     # speed_list=[30]
     acid="A"+str(id)
     bs.stack.stack(f'ORIG {acid} N_1')
     bs.stack.stack(f'DEST {acid} N_4')
-    bs.stack.stack(f'SPD {acid} 30')
+    bs.stack.stack(f'SPD {acid} 35')
     bs.stack.stack(f'ALT {acid} 400')
-    bs.stack.stack(f'ADDWPT {acid} N_2, 400, 40')
-    bs.stack.stack(f'ADDWPT {acid} N_3, 400, 40')
-    bs.stack.stack(f'DUMPRTE {acid}')
-    bs.stack.stack(f'VNAV {acid} ON')
-
 
     f.write(f"00:00:{id}.00>CRE {acid} ELE01 0 0 0 0\n")
     f.write(f"00:00:{id}.00>ORIG {acid} N_1\n")
     f.write(f"00:00:{id}.00>DEST {acid} N_4\n")
-    f.write(f"00:00:{id}.00>SPD {acid} 30\n")
+    f.write(f"00:00:{id}.00>SPD {acid} 35\n")
     f.write(f"00:00:{id}.00>ALT {acid} 400\n")
-    f.write(f"00:00:{id}.00>ADDWPT {acid} N_2, 400, 40\n")
-    f.write(f"00:00:{id}.00>ADDWPT {acid} N_3, 400, 40\n")
+
+    speed=sample(speed_list,1)[0]
+    bs.stack.stack(f'ADDWPT {acid} N_2, 400, {speed}')
+    f.write(f"00:00:{id}.00>ADDWPT {acid} N_2, 400, {speed}\n")
+
+    speed=sample(speed_list,1)[0]
+    bs.stack.stack(f'ADDWPT {acid} N_3, 400, {speed}')
+    f.write(f"00:00:{id}.00>ADDWPT {acid} N_3, 400, {speed}\n")
+
+    bs.stack.stack(f'DUMPRTE {acid}')
+    bs.stack.stack(f'VNAV {acid} ON')
+
     f.write(f"00:00:{id}.00>VNAV {acid} ON\n")
     f.write(f"00:00:{id}.00>DUMPRTE {acid}\n")
 
-
     f.write("\n")
-# set simulation time step, and enable fast-time running
-bs.stack.stack('DT 1;FF')
 
 
-# we'll run the simulation for up to 2000 seconds
 t_max = 3000
-
-ntraf = bs.traf.ntraf
-
 n_steps = int(t_max + 1)
-t = np.linspace(0, t_max, n_steps)
-
-# Generate the demand based on exponential distribution, lambda-number of flight per second, lambda=0.1--flight interval=10s
 ac_number=10
 flight_interval=100
-lambda_x = 1/flight_interval
-ac_demand_interval = [int(expovariate(lambda_x)) for i in range(ac_number)]
-ac_depart_time = np.cumsum(ac_demand_interval)
-ori_depart_time=ac_depart_time.copy()
 departure_safety_bound = 150
-
-# bs.traf.cre(acid="A"+str(0), actype="ELE01",aclat=0,aclon=0.0,acalt=0,acspd=3)
-bs.traf.cre(acid="A"+str(0), actype="ELE01",aclat=0.0, aclon=0.0)
-add_plane(0)
+max_speed = 40
+min_speed = 3
+delta_v = 5
 MAC=0
 LOS=0
 current_ac=0
+
+
+f=open("scenario/interface_naive.scn","w")
+init_bs()
+ac_depart_time,ori_depart_time = generate_demand(flight_interval,ac_number)
+
+
 for i in range(1,n_steps):
     bs.sim.step()
     ac_list=bs.traf.id
@@ -141,22 +149,26 @@ for i in range(1,n_steps):
         if len(lat_list)<=1:
             continue
         else:
-            bs.stack.stack(f"SPD {ac_list[0]} 40")
-            f.write(f"00:00:{i}.00>SPD {ac_list[0]} 40\n")
+            bs.stack.stack(f"SPD {ac_list[0]} {max_speed}")
+            f.write(f"00:00:{i}.00>SPD {ac_list[0]} {max_speed}\n")
             for j in range(len(lat_list)-1):
                 loc1=[lat_list[j],lon_list[j],alt_list[j]]
                 loc2=[lat_list[j+1],lon_list[j+1],alt_list[j+1]]
                 dist=get_distance(loc1,loc2)
+
                 if dist<600:  ## low seperation warning, speed down
-                    bs.stack.stack(f"SPD {ac_list[j+1]} {max(spd_list[j+1]*2-5,3)}")
-                    f.write(f"00:00:{i}.00>SPD {ac_list[j+1]} {max(spd_list[j+1]*2-5,3)}\n")
+                    bs.stack.stack(f"SPD {ac_list[j+1]} {max(spd_list[j+1]*2-delta_v,min_speed)}")
+                    f.write(f"00:00:{i}.00>SPD {ac_list[j+1]} {max(spd_list[j+1]*2-delta_v,min_speed)}\n")
+
                 if dist<100:  ## too low seperation, force hover (nearly)
                     LOS+=1
-                    bs.stack.stack(f"SPD {ac_list[j+1]} 3")
-                    f.write(f"00:00:{i}.00>SPD {ac_list[j+1]} 3\n")
+                    bs.stack.stack(f"SPD {ac_list[j+1]} {min_speed}")
+                    f.write(f"00:00:{i}.00>SPD {ac_list[j+1]} {min_speed}\n")
+
                 if dist>800: ## seperation large, speed up
-                    bs.stack.stack(f"SPD {ac_list[j+1]} {min(spd_list[j+1]*2+5,45)}")
-                    f.write(f"00:00:{i}.00>SPD {ac_list[j+1]} {min(spd_list[j+1]*2+5,45)}\n")
+                    bs.stack.stack(f"SPD {ac_list[j+1]} {min(spd_list[j+1]*2+delta_v,max_speed)}")
+                    f.write(f"00:00:{i}.00>SPD {ac_list[j+1]} {min(spd_list[j+1]*2+delta_v,max_speed)}\n")
+
                 if dist<10:  ## near in-air crash
                     MAC+=1
                     bs.stack.stack(f"DEL {ac_list[j+1]}")
