4345a354ea10bec5e87b3ec04b4290a1878d1c5d
Shulu Chen <rexchen129@gmail.com>
2022-05-04 22:59:06 -0400

    Cross scenarios

--- box_plot.py
+++ box_plot.py
@@ -171,7 +171,9 @@ def plot_NYC():
     plt.savefig('image\\exp4_dalay.png')
 
 def plot_heatmap(demand):
-    data = pd.read_csv(f"result\\grid_search_{demand}.csv")
+    path = f"result\\grid_search_{demand}.txt"
+    data = pd.read_table(path,sep=',',header=None)
+    data.columns=['Number','C','S','Event Type']
     LOS =  data[data['Event Type']=='LOS']
     NMAC = data[data['Event Type']=='NMAC']
     Ground_Delay = data[data['Event Type']=='Ground Delay']
@@ -196,24 +198,29 @@ def plot_heatmap(demand):
     data=pd.DataFrame(table1)
     data=data.pivot("Block Size","Capacity","LOS")
     sns.set(font_scale=1.4)
-    sns.set_context({"figure.figsize":(9,8)})
+    sns.set_context({"figure.figsize":(11,8)})
     # cbar_kws = { 'ticks' : [0, 30] }
-    sns.heatmap(data=data,annot=True,cmap="RdBu_r", vmin=0, vmax=25)
+
+
+    sns.heatmap(data=data,annot=True,cmap="RdBu_r", vmin=0, vmax=25,cbar=True,
+                cbar_kws={'label': 'LOS Duration (%)'})
 
     plt.ylim(0,8)
     # sns.set_context("set_context","Test Class",fontsize=19)
-    # sns.set_ylabel("Average Ground Delay (seconds)",fontsize=19)
+    plt.ylabel('DCB Window Size (seconds)', fontsize = 19)
     plt.savefig(f'image\\heatmap_los_{demand}.png')
+    plt.show()
     plt.close()
 
     table2={"Capacity" : C, "Block Size" : S,"NMAC":NMAC_mean}
     data=pd.DataFrame(table2)
     data=data.pivot("Block Size","Capacity","NMAC")
-    sns.set_context({"figure.figsize":(9,8)})
-    sns.heatmap(data=data,annot=True,cmap="RdBu_r",vmin=0, vmax=5)
+    sns.set_context({"figure.figsize":(11,8)})
+    sns.heatmap(data=data,annot=True,cmap="RdBu_r",vmin=0, vmax=5,cbar=True,
+                cbar_kws={'label': 'NMAC Duration (%)'})
     plt.ylim(0,8)
     # sns.set_xlabel("Capacity",fontsize=10)
-    # sns.set_ylabel("Block Size",fontsize=10)
+    plt.ylabel('DCB Window Size (seconds)', fontsize = 19)
     plt.savefig(f'image\\heatmap_nmac_{demand}.png')
 
 def plot_exp5():
@@ -223,7 +230,7 @@ def plot_exp5():
     plt.yticks(fontsize=19)
     plt.figure(figsize=(10, 8))
     ax = sns.boxplot(x="Test Class", y="Number",
-                 data=Ground_Delay,color="mediumseagreen",showfliers = False)
+                 data=Ground_Delay,color="mediumseagreen",showfliers = True)
     ax.set_xlabel("Test Class",fontsize=10)
     ax.set_ylabel("Average Ground Delay (seconds)",fontsize=10)
     plt.savefig('image\\exp5_delay.png')
@@ -241,7 +248,7 @@ def compute_interval(path):
     NMAC_mean = []
     Ground_Delay_mean = []
 
-    for d in np.arange(10,125,5):
+    for d in np.arange(10,360,10):
         LOS_num = LOS[LOS['D']==d]
         NMAC_num = NMAC[NMAC['D']==d]
         delay_num = Ground_Delay[Ground_Delay['D']==d]
@@ -257,87 +264,93 @@ def compute_interval(path):
 
     # predicted expect and calculate confidence interval
     predicted_expect = np.mean(data, 0)
-    low_CI_bound, high_CI_bound = st.t.interval(0.9, len(LOS_mean) - 1,
+    low_CI_bound, high_CI_bound = st.t.interval(0.95, len(LOS_mean) - 1,
                                                 loc=LOS_mean,scale=LOS_sem)
     # plot confidence interval
-    x = np.linspace(10, 125, num=data_points)
-
+    # x = np.linspace(10, 125, num=data_points)
+    # d = [3600/i for i in x ]
+    x = np.linspace(10, 360, num=data_points)
+    # print(d)
     return LOS_mean,x,low_CI_bound,high_CI_bound
 
 
 
 def plot_exp6():
-    LOS_mean1,X1,low_CI1,high_CI1=compute_interval("result\\interval_none.txt")
-    LOS_mean2,X2,low_CI2,high_CI2=compute_interval("result\\interval_T1.txt")
-    LOS_mean3,X3,low_CI3,high_CI3=compute_interval("result\\interval_T10_2.txt")
-    LOS_mean4,X4,low_CI4,high_CI4=compute_interval("result\\interval_C1S50.txt")
-    LOS_mean5,X5,low_CI5,high_CI5=compute_interval("result\\interval_C1S100.txt")
-    LOS_mean6,X6,low_CI6,high_CI6=compute_interval("result\\interval_C3S150.txt")
-    LOS_mean7,X7,low_CI7,high_CI7=compute_interval("result\\interval_C3S200.txt")
-    LOS_mean8,X8,low_CI8,high_CI8=compute_interval("result\\interval_T5.txt")
-
+    # LOS_mean1,X1,low_CI1,high_CI1=compute_interval("result\\interval_none.txt")
+    # LOS_mean2,X2,low_CI2,high_CI2=compute_interval("result\\interval_T1.txt")
+    # LOS_mean3,X3,low_CI3,high_CI3=compute_interval("result\\interval_T10_2.txt")
+    # LOS_mean4,X4,low_CI4,high_CI4=compute_interval("result\\interval_C1S50.txt")
+    # LOS_mean5,X5,low_CI5,high_CI5=compute_interval("result\\interval_C1S100.txt")
+    # LOS_mean6,X6,low_CI6,high_CI6=compute_interval("result\\interval_C3S150.txt")
+    # LOS_mean7,X7,low_CI7,high_CI7=compute_interval("result\\interval_C3S200.txt")
+    # LOS_mean8,X8,low_CI8,high_CI8=compute_interval("result\\interval_T5.txt")
+    LOS_mean9,X9,low_CI9,high_CI9=compute_interval("result\\demand_none.txt")
+    LOS_mean10,X10,low_CI10,high_CI10=compute_interval("result\\demand_tactical.txt")
+    LOS_mean11,X11,low_CI11,high_CI11=compute_interval("result\\demand_dcb.txt")
     plt.figure(figsize=(16, 12))
     plt.xticks(fontsize=22)
     plt.yticks(fontsize=22)
-    plt.plot(X1,LOS_mean1, linewidth=3., label='No DCB/Tactical')
-    plt.fill_between(X1, low_CI1, high_CI1, alpha=0.5)
+    # plt.plot(X1,LOS_mean1, linewidth=3., label='No DCB/Tactical')
+    # plt.fill_between(X1, low_CI1, high_CI1, alpha=0.5)
 
-    # plt.plot(X2,LOS_mean2, linewidth=3., label='Tactical, control frequency = 1s')
-    # plt.fill_between(X2, low_CI2, high_CI2, alpha=0.5)
+    plt.plot(X9,LOS_mean9, linewidth=3., label='No DCB/Tactical')
+    plt.fill_between(X9, low_CI9, high_CI9, alpha=0.5)
 
-    # plt.plot(X8,LOS_mean8, linewidth=3., label='Tactical, control frequency = 5s')
-    # plt.fill_between(X8, low_CI8, high_CI8, alpha=0.5)
+    plt.plot(X10,LOS_mean10, linewidth=3., label='Tactical deconfliction')
+    plt.fill_between(X10, low_CI10, high_CI10, alpha=0.5)
 
-    plt.plot(X3,LOS_mean3, linewidth=3., label='Tactical mitigation')
-    plt.fill_between(X3, low_CI3, high_CI3, alpha=0.5)
-
-    plt.plot(X7,LOS_mean7, linewidth=3., label='DCB+Tactical,C=3,S=200')
-    plt.fill_between(X7, low_CI7, high_CI7, alpha=0.5)
+    plt.plot(X11,LOS_mean11, linewidth=3., label='DCB+Tactical,C=3,S=200')
+    plt.fill_between(X11, low_CI11, high_CI11, alpha=0.5)
+    #
+    # plt.plot(X7,LOS_mean7, linewidth=3., label='DCB+Tactical,C=3,S=200')
+    # plt.fill_between(X7, low_CI7, high_CI7, alpha=0.5)
 
     plt.ylabel('LOS Duration (%)', fontdict={'size': 22})
-    plt.xlabel('Average Departure Interval', fontdict={'size': 22})
-    plt.ylim(0,35)
+    plt.xlabel('Demand (ops/hr)', fontdict={'size': 22})
+    plt.ylim(0,40)
+    plt.xlim(10,360)
     plt.legend(fontsize=22)
     # plt.grid()
     plt.savefig('image\\exp6_LOS.png')
     plt.show()
     plt.close()
 
-    plt.figure(figsize=(16, 12))
-    plt.xticks(fontsize=22)
-    plt.yticks(fontsize=22)
-
-    plt.plot(X1,LOS_mean1, linewidth=3., label='No DCB/Tactical')
-
-    plt.plot(X2,LOS_mean2, linewidth=3., label='Tactical, control frequency: 1s')
-
-    plt.plot(X8,LOS_mean8, linewidth=3., label='Tactical, control frequency: 5s')
-
-    plt.plot(X3,LOS_mean3, linewidth=3., label='Tactical, control frequency: 10s')
-
-    plt.plot(X4,LOS_mean4, linewidth=3., label='DCB+Tactical,C=1,S=50')
-
-    plt.plot(X5,LOS_mean5, linewidth=3., label='DCB+Tactical,C=1,S=100')
-
-    plt.plot(X6,LOS_mean6, linewidth=3., label='DCB+Tactical,C=3,S=150')
-
-    plt.plot(X7,LOS_mean7, linewidth=3., label='DCB+Tactical,C=3,S=200')
-
-    plt.ylabel('LOS Duration (%)', fontdict={'size': 22})
-    plt.xlabel('Average Departure Interval', fontdict={'size': 22})
-    plt.ylim(0,35)
-    plt.legend(fontsize=22)
-    # plt.grid()
-    plt.savefig('image\\exp6_multiple.png')
-    plt.show()
-    plt.close()
+    # plt.figure(figsize=(16, 12))
+    # plt.xticks(fontsize=22)
+    # plt.yticks(fontsize=22)
+    #
+    # plt.plot(X1,LOS_mean1, linewidth=3., label='No DCB/Tactical')
+    #
+    # plt.plot(X2,LOS_mean2, linewidth=3., label='Tactical, control frequency: 1s')
+    #
+    # plt.plot(X8,LOS_mean8, linewidth=3., label='Tactical, control frequency: 5s')
+    #
+    # plt.plot(X3,LOS_mean3, linewidth=3., label='Tactical, control frequency: 10s')
+    #
+    # plt.plot(X4,LOS_mean4, linewidth=3., label='DCB+Tactical,C=1,S=50')
+    #
+    # plt.plot(X5,LOS_mean5, linewidth=3., label='DCB+Tactical,C=1,S=100')
+    #
+    # plt.plot(X6,LOS_mean6, linewidth=3., label='DCB+Tactical,C=3,S=150')
+    #
+    # plt.plot(X7,LOS_mean7, linewidth=3., label='DCB+Tactical,C=3,S=200')
+    #
+    # plt.ylabel('LOS Duration (%)', fontdict={'size': 22})
+    # plt.xlabel('Demand (ops/hr)', fontdict={'size': 22})
+    # plt.ylim(0,40)
+    # plt.xlim(20,360)
+    # plt.legend(fontsize=22)
+    # # plt.grid()
+    # # plt.savefig('image\\exp6_multiple.png')
+    # plt.show()
+    # plt.close()
 
 
 # plot_capacity()
 # plot_block()
 # plot_interval()
 # plot_NYC()
-# plot_heatmap(30)
+# plot_heatmap(120)
 # plot_exp5()
 plot_exp6()
 
@@ -346,7 +359,7 @@ plot_exp6()
 
 
 
-
+# LOS_mean1,X1,low_CI1,high_CI1=compute_interval("result\\interval_none.txt")
 
 
 
--- sim_run.py
+++ sim_run.py
@@ -8,26 +8,31 @@
 '''
 import os
 import sys
+import numpy as np
 # C =  int(sys.argv[1])
 # S = int(sys.argv[2])
 C_list = [1,2,3,4,5,6,7,8,20]
 S_list = [50,100,150,200,250,300,350,400]
-exp = 1
+exp = 6
 if exp ==1:
     print("Experiment 1")
     for c in C_list:
         for s in S_list:
             os.system(f"python exp1.py {c} {s}")
-
 elif exp ==2:
     print("Experiment 2")
     for i in range(20):
         os.system(f"python exp2.py {para}")
 elif exp ==3:
     print("Experiment 3")
-    for i in range(20):
+    for para in np.arange(10,360,10):
         os.system(f"python exp3.py {para}")
 elif exp ==4:
     print("Experiment 4")
     for i in range(20):
         os.system(f"python exp4.py {para}")
+elif exp ==6:
+    print("Cross heatmap")
+    for c in C_list:
+        for s in S_list:
+            os.system(f"python exp6_cross.py {c} {s}")
