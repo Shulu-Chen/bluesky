72795b0847b1af4addf46b9205c09378c629c6e4
Shulu Chen <rexchen129@gmail.com>
2022-05-07 23:56:46 -0400

    plot_function

--- box_plot.py
+++ box_plot.py
@@ -173,8 +173,22 @@ def plot_NYC():
 def plot_heatmap(demand,cross):
     if not cross:
         path = f"result\\grid_search_{demand}.txt"
+        running_time = 2500
+        los_max = 25
+        nmac_max = 5
     else:
         path = f"result\\GridSearch_cross_{demand}.txt"
+        running_time = 2000
+        los_max = 10
+        nmac_max = 1
+
+    if demand == 30:
+        colabel = True
+        wide = 11
+    else:
+        colabel = False
+        wide = 9
+
     data = pd.read_table(path,sep=',',header=None)
     data.columns=['Number','C','S','Event Type']
     LOS =  data[data['Event Type']=='LOS']
@@ -192,45 +206,61 @@ def plot_heatmap(demand,cross):
             LOS_num = LOS[(LOS['C']==c) & (LOS['S']==s)]
             NMAC_num = NMAC[(NMAC['C']==c) & (NMAC['S']==s)]
             delay_num = Ground_Delay[(Ground_Delay['C']==c) & (Ground_Delay['S']==s)]
-            LOS_mean.append(round(np.mean(LOS_num['Number'])*5/3000,1))
-            NMAC_mean.append(round(np.mean(NMAC_num['Number'])*5/3000,1))
-            Ground_Delay_mean.append(np.mean(delay_num['Number']))
+            LOS_mean.append(round(np.mean(LOS_num['Number'])*5/running_time,1))
+            NMAC_mean.append(round(np.mean(NMAC_num['Number'])*5/running_time,1))
+            Ground_Delay_mean.append(round(np.mean(delay_num['Number'])/60,1))
             C.append(c)
             S.append(s)
+
     table1={"Capacity" : C, "Block Size" : S,"LOS":LOS_mean}
     data=pd.DataFrame(table1)
     data=data.pivot("Block Size","Capacity","LOS")
     sns.set(font_scale=1.4)
-    sns.set_context({"figure.figsize":(11,8)})
+    sns.set_context({"figure.figsize":(wide,8)})
     # cbar_kws = { 'ticks' : [0, 30] }
-
-
-    sns.heatmap(data=data,annot=True,cmap="RdBu_r", vmin=0, vmax=7,cbar=True,
+    sns.heatmap(data=data,annot=True,cmap="RdBu_r", vmin=0, vmax=los_max,cbar=colabel,
                 cbar_kws={'label': 'LOS Duration (%)'})
 
     plt.ylim(0,8)
     # sns.set_context("set_context","Test Class",fontsize=19)
     plt.ylabel('DCB Window Size (seconds)', fontsize = 19)
     if not cross:
-        plt.savefig(f'image\\heatmap_merge_los_{demand}.png')
+        plt.savefig(f'image\\heatmap_merge_los_{demand}.pdf')
     else:
-        plt.savefig(f'image\\heatmap_cross_los_{demand}.png')
+        plt.savefig(f'image\\heatmap_cross_los_{demand}.pdf')
     plt.show()
     plt.close()
+
 
     table2={"Capacity" : C, "Block Size" : S,"NMAC":NMAC_mean}
     data=pd.DataFrame(table2)
     data=data.pivot("Block Size","Capacity","NMAC")
-    sns.set_context({"figure.figsize":(11,8)})
-    sns.heatmap(data=data,annot=True,cmap="RdBu_r",vmin=0, vmax=0.9,cbar=True,
+    sns.set_context({"figure.figsize":(wide,8)})
+    sns.heatmap(data=data,annot=True,cmap="RdBu_r",vmin=0, vmax=nmac_max,cbar=colabel,
                 cbar_kws={'label': 'NMAC Duration (%)'})
     plt.ylim(0,8)
     # sns.set_xlabel("Capacity",fontsize=10)
     plt.ylabel('DCB Window Size (seconds)', fontsize = 19)
     if not cross:
-        plt.savefig(f'image\\heatmap_merge_nmac_{demand}.png')
+        plt.savefig(f'image\\heatmap_merge_nmac_{demand}.pdf')
     else:
-        plt.savefig(f'image\\heatmap_cross_nmac_{demand}.png')
+        plt.savefig(f'image\\heatmap_cross_nmac_{demand}.pdf')
+    plt.show()
+    plt.close()
+
+    table3={"Capacity" : C, "Block Size" : S,"Delay":Ground_Delay_mean}
+    data=pd.DataFrame(table3)
+    data=data.pivot("Block Size","Capacity","Delay")
+    sns.set_context({"figure.figsize":(wide,8)})
+    sns.heatmap(data=data,annot=True,cmap="OrRd",vmin=0, vmax=15,cbar=colabel,
+                cbar_kws={'label': 'Average Ground Delay (minute)'})
+    plt.ylim(0,8)
+    # sns.set_xlabel("Capacity",fontsize=10)
+    plt.ylabel('DCB Window Size (seconds)', fontsize = 19)
+    if not cross:
+        plt.savefig(f'image\\heatmap_merge_delay_{demand}.pdf')
+    else:
+        plt.savefig(f'image\\heatmap_cross_delay_{demand}.pdf')
     plt.show()
 
 def plot_exp5():
@@ -245,7 +275,13 @@ def plot_exp5():
     ax.set_ylabel("Average Ground Delay (seconds)",fontsize=10)
     plt.savefig('image\\exp5_delay.png')
 
-def compute_interval(path,plt_LOS):
+def compute_interval(path,plt_LOS,scn):
+    if scn == "cross":
+        running_time = 2000
+    if scn == "merge":
+        running_time = 2500
+    if scn == "NYC":
+        running_time = 3000
     data = pd.read_table(path,sep=',',header=None)
     data.columns=['N','D','Type']
     LOS =  data[data['Type']=='LOS']
@@ -262,8 +298,8 @@ def compute_interval(path,plt_LOS):
         LOS_num = LOS[LOS['D']==d]
         NMAC_num = NMAC[NMAC['D']==d]
         delay_num = Ground_Delay[Ground_Delay['D']==d]
-        LOS_mean.append(np.mean(LOS_num['N']*5/3000))
-        LOS_sem.append(st.sem(LOS_num['N']*5/3000))
+        LOS_mean.append(np.mean(LOS_num['N']*5/running_time))
+        LOS_sem.append(st.sem(LOS_num['N']*5/running_time))
         Ground_Delay_mean.append(np.mean(delay_num['N']))
         Ground_Delay_sem.append(st.sem(delay_num['N']))
         # LOS_std.append(round(np.std(LOS_num['N'])*5/3000,1))
@@ -292,106 +328,62 @@ def compute_interval(path,plt_LOS):
 
 
 
-def plot_exp6():
-    # LOS_mean1,X1,low_CI1,high_CI1=compute_interval("result\\interval_none.txt")
-    # LOS_mean2,X2,low_CI2,high_CI2=compute_interval("result\\interval_T1.txt")
-    # LOS_mean3,X3,low_CI3,high_CI3=compute_interval("result\\interval_T10_2.txt")
-    # LOS_mean4,X4,low_CI4,high_CI4=compute_interval("result\\interval_C1S50.txt")
-    # LOS_mean5,X5,low_CI5,high_CI5=compute_interval("result\\interval_C1S100.txt")
-    # LOS_mean6,X6,low_CI6,high_CI6=compute_interval("result\\interval_C3S150.txt")
-    # LOS_mean7,X7,low_CI7,high_CI7=compute_interval("result\\interval_C3S200.txt")
-    # LOS_mean8,X8,low_CI8,high_CI8=compute_interval("result\\interval_T5.txt")
-    # LOS_mean9,X9,low_CI9,high_CI9=compute_interval("result\\demand_none.txt")
-    # LOS_mean10,X10,low_CI10,high_CI10=compute_interval("result\\demand_tactical.txt")
-    # LOS_mean11,X11,low_CI11,high_CI11=compute_interval("result\\demand_dcb.txt")
-    # LOS_mean12,X12,low_CI12,high_CI12=compute_interval("result\\demand_cross_none.txt")
-    # LOS_mean13,X13,low_CI13,high_CI13=compute_interval("result\\demand_cross_tactical.txt")
-    # LOS_mean14,X14,low_CI14,high_CI14=compute_interval("result\\demand_cross_C3S200.txt")
-    # LOS_mean15,X15,low_CI15,high_CI15=compute_interval("result\\demand_cross_C4S200.txt")
-    # LOS_mean16,X16,low_CI16,high_CI16=compute_interval("result\\NYC_data.txt",False)
-    # LOS_mean17,X17,low_CI17,high_CI17=compute_interval("result\\demand_cross_C7S200.txt",False)
-    LOS_mean18,X18,low_CI18,high_CI18=compute_interval("result/NYC_data_DCB_C3C7.txt",False)
-    LOS_mean19,X19,low_CI19,high_CI19=compute_interval("result/NYC_data_none.txt",False)
+def plot_exp6(plt_LOS,scn):
+
+    if scn =="merge":
+        LOS_mean1,X1,low_CI1,high_CI1=compute_interval("result\\demand_merge_none.txt",plt_LOS,scn)
+        LOS_mean2,X2,low_CI2,high_CI2=compute_interval("result\\demand_merge_tactical.txt",plt_LOS,scn)
+        LOS_mean3,X3,low_CI3,high_CI3=compute_interval("result\\demand_merge_dcb.txt",plt_LOS,scn)
+        label_name = "$C=3,S=200$"
+    if scn =="cross":
+        LOS_mean1,X1,low_CI1,high_CI1=compute_interval("result\\demand_cross_none.txt",plt_LOS,scn)
+        LOS_mean2,X2,low_CI2,high_CI2=compute_interval("result\\demand_cross_tactical.txt",plt_LOS,scn)
+        LOS_mean3,X3,low_CI3,high_CI3=compute_interval("result\\demand_cross_C7S200.txt",plt_LOS,scn)
+        label_name = "$C=3,S=200$"
+    if scn == "NYC":
+        LOS_mean1,X1,low_CI1,high_CI1=compute_interval("result/NYC_data_none.txt",plt_LOS,scn)
+        LOS_mean2,X2,low_CI2,high_CI2=compute_interval("result/NYC_data_tac.txt",plt_LOS,scn)
+        LOS_mean3,X3,low_CI3,high_CI3=compute_interval("result/NYC_data_DCB_C3C7.txt",plt_LOS,scn)
+        label_name = "$C_c=3,C_m=7,S=200$"
     plt.figure(figsize=(16, 12))
     plt.xticks(fontsize=22)
     plt.yticks(fontsize=22)
-    # plt.plot(X1,LOS_mean1, linewidth=3., label='No DCB/Tactical')
-    # plt.fill_between(X1, low_CI1, high_CI1, alpha=0.5)
 
-    # plt.plot(X9,LOS_mean9, linewidth=3., label='No DCB/Tactical')
-    # plt.fill_between(X9, low_CI9, high_CI9, alpha=0.5)
-    #
-    # plt.plot(X10,LOS_mean10, linewidth=3., label='Tactical deconfliction')
-    # plt.fill_between(X10, low_CI10, high_CI10, alpha=0.5)
-    #
-    # plt.plot(X11,LOS_mean11, linewidth=3., label='DCB+Tactical,C=3,S=200')
-    # plt.fill_between(X11, low_CI11, high_CI11, alpha=0.5)
+    plt.plot(X1,LOS_mean1, linewidth=3., label='No DCB/tactical deconfliction')
+    plt.fill_between(X1, low_CI1, high_CI1, alpha=0.5)
 
+    plt.plot(X2,LOS_mean2, linewidth=3., label='Tactical deconfliction')
+    plt.fill_between(X2, low_CI2, high_CI2, alpha=0.5)
 
-    # plt.plot(X12,LOS_mean12, linewidth=3., label='No DCB/Tactical')
-    # plt.fill_between(X12, low_CI12, high_CI12, alpha=0.5)
-    #
-    # plt.plot(X13,LOS_mean13, linewidth=3., label='Tactical deconfliction')
-    # plt.fill_between(X13, low_CI13, high_CI13, alpha=0.5)
+    plt.plot(X3,LOS_mean3, linewidth=3., label=f'DCB+Tactical,{label_name}')
+    plt.fill_between(X3, low_CI3, high_CI3, alpha=0.5)
 
-    # plt.plot(X17,LOS_mean17, linewidth=3., label='DCB+Tactical,C=7,S=200')
-    # plt.fill_between(X17, low_CI17, high_CI17, alpha=0.5)
-    #
-    plt.plot(X19,LOS_mean19, linewidth=3., label='No DCB/tactical')
-    plt.fill_between(X19, low_CI19, high_CI19, alpha=0.5)
+    if plt_LOS:
+        plt.ylabel('LOS Duration (%)', fontdict={'size': 22})
+        plt_name = "LOS"
+    else:
+        plt_name = "delay"
+        plt.ylabel('Average Ground Delay (seconds)', fontdict={'size': 22})
 
-    plt.plot(X18,LOS_mean18, linewidth=3., label='DCB+Tactical,C=7')
-    plt.fill_between(X18, low_CI18, high_CI18, alpha=0.5)
-
-    # plt.ylabel('Average Ground Delay (seconds)', fontdict={'size': 22})
-    plt.ylabel('LOS Duration (%)', fontdict={'size': 22})
     plt.xlabel('Demand (ops/hr)', fontdict={'size': 22})
-    # plt.ylim(0,25)
+
+    # plt.ylim(0,100)
     plt.xlim(30,360)
     plt.legend(loc = "upper left",fontsize=22)
-    # plt.grid()
-    # plt.savefig('image\\exp6_LOS_cross.png')
+
+    plt.savefig(f'image\\exp6_{plt_name}_{scn}.pdf')
     plt.show()
     plt.close()
 
-    # plt.figure(figsize=(16, 12))
-    # plt.xticks(fontsize=22)
-    # plt.yticks(fontsize=22)
-    #
-    # plt.plot(X1,LOS_mean1, linewidth=3., label='No DCB/Tactical')
-    #
-    # plt.plot(X2,LOS_mean2, linewidth=3., label='Tactical, control frequency: 1s')
-    #
-    # plt.plot(X8,LOS_mean8, linewidth=3., label='Tactical, control frequency: 5s')
-    #
-    # plt.plot(X3,LOS_mean3, linewidth=3., label='Tactical, control frequency: 10s')
-    #
-    # plt.plot(X4,LOS_mean4, linewidth=3., label='DCB+Tactical,C=1,S=50')
-    #
-    # plt.plot(X5,LOS_mean5, linewidth=3., label='DCB+Tactical,C=1,S=100')
-    #
-    # plt.plot(X6,LOS_mean6, linewidth=3., label='DCB+Tactical,C=3,S=150')
-    #
-    # plt.plot(X7,LOS_mean7, linewidth=3., label='DCB+Tactical,C=3,S=200')
-    #
-    # plt.ylabel('LOS Duration (%)', fontdict={'size': 22})
-    # plt.xlabel('Demand (ops/hr)', fontdict={'size': 22})
-    # plt.ylim(0,40)
-    # plt.xlim(20,360)
-    # plt.legend(fontsize=22)
-    # # plt.grid()
-    # # plt.savefig('image\\exp6_multiple.png')
-    # plt.show()
-    # plt.close()
 
 
 # plot_capacity()
 # plot_block()
 # plot_interval()
 # plot_NYC()
-# plot_heatmap(30,True)
+# plot_heatmap(30,False) #True: Cross, False: merge
 # plot_exp5()
-plot_exp6()
+plot_exp6(True,"NYC")  #True: LOS, False: delay
 
 
 
