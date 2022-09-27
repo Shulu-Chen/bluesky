2bd24f15e6b06ba12de0098fa342471acee9c7c2
Shulu Chen <rexchen129@gmail.com>
2022-09-27 14:30:01 -0400

    arrange files

--- experiments/interface_Y.py
+++ experiments/interface_Y.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 # coding: utf-8
 '''
- @Time    : 4/28/2022 6:47 PM
+ @Time    : 2/22/2022 10:08 PM
  @Author  : Shulu Chen
- @FileName: exp1.py
+ @FileName: interface_naive.py
  @Software: PyCharm
 '''
+
 import numpy as np
 import random
 import bluesky as bs
+from bluesky.simulation import ScreenIO
 from geopy.distance import geodesic
 from math import radians, cos, sin, asin, sqrt
 import matplotlib.pyplot as plt
@@ -16,6 +18,16 @@ from random import expovariate,seed
 import itertools
 from tqdm import tqdm
 import sys
+###
+class ScreenDummy(ScreenIO):
+    """
+    Dummy class for the screen. Inherits from ScreenIO to make sure all the
+    necessary methods are there. This class is there to reimplement the echo
+    method so that console messages are printed.
+    """
+    def echo(self, text='', flags=0):
+        """Just print echo messages"""
+        print("BlueSky console:", text)
 
 #conpute the distance of two aircraft, meters
 def get_distance(location1,location2):
@@ -41,10 +53,26 @@ def generate_interval(interval,number):
 
 def init_bs():
 
+    # initialize bluesky as non-networked simulation node
+
+    bs.stack.stack('CRELOG Y 1')
+    bs.stack.stack('Y ADD id,lat, lon, alt, tas, vs ')
+    bs.stack.stack('Y ON 1  ')
+    # bs.stack.stack('ASAS ON')
     bs.stack.stack('TAXI OFF 4')
+    # f.write("00:00:00.00>CRELOG Y_gui 1\n")
+    # f.write("00:00:00.00>Y_gui ADD id,lat, lon, alt, tas, vs\n")
+    # f.write("00:00:00.00>Y_gui ON 1\n")
+    f.write("00:00:00.00>TRAILS ON \n")
+    f.write("00:00:00.00>TAXI OFF 4\n")
+    # f.write("0:00:00.00>ASAS ON \n")
+    f.write("0:00:00.00>PAN 0,0.2 \n")
+    f.write("0:00:00.00>ZOOM 3 \n")
+    f.write("00:00:00.00>CIRCLE a, 0.0,0.0 0.2\n")
+    # f.write("0:00:00.00>FF \n")
+    f.write("\n")
 
     # set simulation time step, and enable fast-time running
-
     bs.stack.stack('DT 1;FF')
     bs.traf.cre(acid="U"+str(0), actype="ELE01",aclat=0.1, aclon=-0.1)
     add_plane(0,"U")
@@ -59,20 +87,31 @@ def add_plane(id,type):
 
         bs.stack.stack(f'ORIG {acid} N_7')
         bs.stack.stack(f'DEST {acid} N_4')
+        f.write(f"00:00:{id}.00>CRE {acid} ELE01 0.1 -0.1 0 0\n")
+        f.write(f"00:00:{id}.00>ORIG {acid} N_7\n")
+        f.write(f"00:00:{id}.00>DEST {acid} N_4\n")
 
         v = random.choice(speed_list)
         bs.stack.stack(f'SPD {acid} {v}')
+        f.write(f"00:00:{id}.00>SPD {acid} {v}\n")
+
         bs.stack.stack(f'ALT {acid} 400')
+        f.write(f"00:00:{id}.00>ALT {acid} 400\n")
 
         v = random.choice(speed_list)
         bs.stack.stack(f'ADDWPT {acid} N_1, 400, {v}')
+        f.write(f"00:00:{id}.00>ADDWPT {acid} N_1, 400, {v}\n")
 
         v = random.choice(speed_list)
         bs.stack.stack(f'ADDWPT {acid} N_2, 400, {v}')
+        f.write(f"00:00:{id}.00>ADDWPT {acid} N_2, 400, {v}\n")
 
         v = random.choice(speed_list)
         bs.stack.stack(f'ADDWPT {acid} N_3, 400, {v}')
+        f.write(f"00:00:{id}.00>ADDWPT {acid} N_3, 400, {v}\n")
+
         bs.stack.stack(f'VNAV {acid} ON')
+        f.write(f"00:00:{id}.00>VNAV {acid} ON\n")
 
     if type=="D":
 
@@ -81,48 +120,58 @@ def add_plane(id,type):
         bs.stack.stack(f'ORIG {acid} N_9')
         bs.stack.stack(f'DEST {acid} N_4')
 
+        f.write(f"00:00:{id}.00>CRE {acid} ELE01 -0.1 -0.1 0 0\n")
+        f.write(f"00:00:{id}.00>ORIG {acid} N_9\n")
+        f.write(f"00:00:{id}.00>DEST {acid} N_4\n")
+
         v = random.choice(speed_list)
         bs.stack.stack(f'SPD {acid} {v}')
+        f.write(f"00:00:{id}.00>SPD {acid} {v}\n")
 
         bs.stack.stack(f'ALT {acid} 400')
+        f.write(f"00:00:{id}.00>ALT {acid} 400\n")
 
         v = random.choice(speed_list)
         bs.stack.stack(f'ADDWPT {acid} N_1, 400, {v}')
+        f.write(f"00:00:{id}.00>ADDWPT {acid} N_1, 400, {v}\n")
 
         v = random.choice(speed_list)
         bs.stack.stack(f'ADDWPT {acid} N_2, 400, {v}')
+        f.write(f"00:00:{id}.00>ADDWPT {acid} N_2, 400, {v}\n")
 
         v = random.choice(speed_list)
         bs.stack.stack(f'ADDWPT {acid} N_3, 400, {v}')
+        f.write(f"00:00:{id}.00>ADDWPT {acid} N_3, 400, {v}\n")
 
-
+        f.write(f"00:00:{id}.00>VNAV {acid} ON\n")
         bs.stack.stack(f'VNAV {acid} ON')
 
-
+    f.write("\n")
 
 t_max = 10000                   #second
 n_steps = int(t_max + 1)
-# inv = int(sys.argv[1])
-inv = 90
+inv = 10
 AC_nums = [10,10]
 # AC_intervals = [60,60]         #second
 AC_intervals =[inv,inv]
-departure_safety_bound = 150   #second
+departure_safety_bound = 10   #second
 max_speed = 40                 #kts
 min_speed = 3                  #kts
 delta_v = 5                    #kts
-check_inv = 1                  #second
-control_inv = 5               #second
+check_inv = 100                  #second
+control_inv = 100               #second
 NMAC_dist = 10                 #meter
 LOS_dist = 100                 #meter
 Warning_dist = 600             #meter
 SpeedUp_dist = 800
-merge_capacity = int(sys.argv[1])
+merge_capacity = 10
 merge_time = 1015              #second
-check_block_size =  int(sys.argv[2])       #second
+check_block_size =  100        #second
 
 
-bs.init('sim-detached')
+f=open("../scenario/interface_Y.scn","w")
+
+bs.init(mode='sim', detached=True)
 
 def run_sim(check_point_capacity,block_size,number_list=AC_nums,interval_list=AC_intervals):
     init_bs()
@@ -227,10 +276,13 @@ def run_sim(check_point_capacity,block_size,number_list=AC_nums,interval_list=AC
                             U_depart_time[U_current_ac:]=list(map(lambda x:x+1,U_depart_time[U_current_ac:]))
 
         # ## in-air deconfliction ##
-        if i%control_inv==0:
+        if i%check_inv==0:
             if len(lat_list)<=1 or len(lat_list)<len(ac_list):
                 continue
             else:
+                ## For speed, input is kts, api output is m/s, the rate is 1.95
+                # bs.stack.stack(f"SPD {ac_list[0]} {min(spd_list[0]*1.93+delta_v,max_speed)}")
+                # f.write(f"00:00:{i}.00>SPD {ac_list[0]} {min(spd_list[0]*1.93+delta_v,max_speed)}\n")
 
                 dist_list=[]
                 ac_comb = list(itertools.combinations(ac_list, 2))
@@ -264,7 +316,7 @@ def run_sim(check_point_capacity,block_size,number_list=AC_nums,interval_list=AC
                             if operate_dist>SpeedUp_dist:
                                 operate_ac = ac_list.index(operate_dic[operated_comb])
                                 bs.stack.stack(f"SPD {ac_list[operate_ac]} {min(spd_list[operate_ac]*1.93+delta_v,max_speed)}")
-
+                                f.write(f"00:00:{i}.00>SPD {ac_list[operate_ac]} {min(spd_list[operate_ac]*1.93+delta_v,max_speed)}\n")
                                 if spd_list[operate_ac]*1.93+delta_v >= max_speed:
                                     pop_list.append(operated_comb)
                         except:
@@ -296,46 +348,33 @@ def run_sim(check_point_capacity,block_size,number_list=AC_nums,interval_list=AC
 
                         if dist<Warning_dist:  ## low seperation warning, speed down
                             bs.stack.stack(f"SPD {ac_list[operate_ac]} {min(max(spd_list[operate_ac]*1.93-delta_v,min_speed),max_speed)}")
-
+                            f.write(f"00:00:{i}.00>SPD {ac_list[operate_ac]} {min(max(spd_list[operate_ac]*1.93-delta_v,min_speed),max_speed)}\n")
 
                         if dist<LOS_dist:  ## too low seperation, force hover (nearly)
-                            # LOS+=1
+                            LOS+=1
                             bs.stack.stack(f"SPD {ac_list[operate_ac]} {min_speed}")
+                            f.write(f"00:00:{i}.00>SPD {ac_list[operate_ac]} {min_speed}\n")
 
-
-        if i%check_inv==0:
-            if len(lat_list)<=1 or len(lat_list)<len(ac_list):
-                continue
-            else:
-
-                dist_list=[]
-                ac_comb = list(itertools.combinations(ac_list, 2))
-
-                for acs in ac_comb:
-                    ac1 = ac_list.index(acs[0])
-                    ac2 = ac_list.index(acs[1])
-                    loc1=[lat_list[ac1],lon_list[ac1],alt_list[ac1]]
-                    loc2=[lat_list[ac2],lon_list[ac2],alt_list[ac2]]
-                    dist_list.append(get_distance(loc1,loc2))
-
-                dist_list = np.array(dist_list)
-                LOS_temp = np.sum(dist_list < LOS_dist)
-                NMAC_temp = np.sum(dist_list < NMAC_dist)
-                LOS += (LOS_temp-NMAC_temp)
-                NMAC += NMAC_temp
-
+                        if dist<NMAC_dist:  ## near in-air crash
+                            NMAC+=1
+                            # bs.stack.stack(f"DEL {ac_list[operate_ac]}")
+                            # bs.stack.stack(f"DEL {ac_list[keep_ac]}")
+                            # f.write(f"00:00:{i}.00>DEL {ac_list[operate_ac]}\n")
+                            # f.write(f"00:00:{i}.00>DEL {ac_list[keep_ac]}\n")
+    bs.stack.stack('STOP')
     return [LOS,NMAC],(np.mean(U_depart_time-U_depart_time_ori)+np.mean(D_depart_time-D_depart_time_ori))/2
 
+LOS_list = []
+delay_list= []
 
 safety,efficiency = run_sim(merge_capacity,check_block_size)
 print("*******************************")
 print(f"number of LOS:{safety[0]}")
 print(f"number of MAC:{safety[1]}")
 print(f"average delay:{round(efficiency)} s")
-print("Capacity=",merge_capacity)
-print("Block size=",check_block_size)
+print("Flight interval=",inv)
 print("*******************************")
-g=open("../result/grid_search_40.txt", "a")
-g.write(f"{safety[0]},{merge_capacity},{check_block_size},LOS\n")
-g.write(f"{safety[1]},{merge_capacity},{check_block_size},NMAC\n")
-g.write(f"{round(efficiency)},{merge_capacity},{check_block_size},Ground Delay\n")
+g=open("result\\Interval data.txt","a")
+g.write(f"{safety[0]},{inv},LOS\n")
+g.write(f"{safety[1]},{inv},NMAC\n")
+g.write(f"{round(efficiency)},{inv},Ground Delay\n")
--- git_test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from random import expovariate
-l=[]
-
-ac_number=10
-lambda_x = 0.1
-ac_demand_interval = [int(expovariate(lambda_x)) for i in range(ac_number)]
-print(ac_demand_interval)
